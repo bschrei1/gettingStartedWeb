6ec373f96db05c909d2b8f56a72683f42dd2d972
Ben Schreiber <benschreiber93@gmail.com>
2016-12-20 15:51:04 -0500

    now server renders text

--- server.js
+++ server.js
@@ -3,7 +3,25 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: process.env.PORT ||3000 });
+server.connection({ port: process.env.PORT||8000 });
+
+
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+//            reply.file('./index.html');
+            reply("hello");
+        }
+    });
+});
 
 server.start((err) => {
 
@@ -12,4 +30,19 @@ server.start((err) => {
     }
     console.log(`Server running at: ${server.info.uri}`);
 });
+
+/*'use strict';
+
+const Hapi = require('hapi');
+
+const server = new Hapi.Server();
+server.connection({ port: process.env.PORT ||3000 });
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+});*/
 
