0105ebeb07fce00660d6cc2b431641fb247cd9f7
Ben Schreiber <benschreiber93@gmail.com>
2016-12-20 15:43:41 -0500

    made server the most simple

--- server.js
+++ server.js
@@ -3,25 +3,7 @@
 const Hapi = require('hapi');
 
 const server = new Hapi.Server();
-server.connection({ port: 8000 });
-
-
-
-server.register(require('inert'), (err) => {
-
-    if (err) {
-        throw err;
-    }
-
-    server.route({
-        method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-//            reply.file('./index.html');
-            reply("hello");
-        }
-    });
-});
+server.connection({ port: 3000 });
 
 server.start((err) => {
 
