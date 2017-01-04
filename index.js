f0c7e8e77c940c92ccbd6c12ed2412292654cbf2
Ben Schreiber <benschreiber93@gmail.com>
2017-01-04 17:09:57 -0500

    can read from styles.css and has jquery now

---
+++ index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+
+   // jQuery methods go here...
+     $("#button").click(function(){
+        $("#toggler").toggleClass('hidden');
+    });
+
+});
--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Path = require('path');
 
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname,'')
+            }
+        }
+    }
+});
 server.connection({ port: process.env.PORT||8000 });
 
 
@@ -15,21 +24,67 @@ server.register(require('inert'), (err) => {
 
     server.route({
         method: 'GET',
-        path: '/hello',
+        path: '/',
         handler: function (request, reply) {
-//            reply.file('./index.html');
-            reply("hello dude");
+            reply.file('./index.html');
+            //reply("hello dude");
         }
     });
     
+   /*server.route({
+       method: 'GET',
+        path: '/hello',   // http://localhost:8000/other.html
+         handler: function (request, reply) {
+           directory: {
+               path: ".",
+               listing: false
+           }
+        }
+    });*/
+    
     server.route({
         method: 'GET',
-        path: '/',
-        handler: function (request, reply) {
-//            reply.file('./index.html');
-            reply.file("./index.html");
+        path: '/styles.css',
+        handler: {
+            file: './styles.css'
         }
     });
+    server.route({
+        method: 'GET',
+        path: '/index.js',
+        handler: {
+            file: './index.js'
+        }
+    });
+    server.route({
+        method: 'GET',
+        path: '/node_modules/jquery-3.1.1.min.js',
+        handler: {
+            file: './node_modules/jquery-3.1.1.min.js'
+        }
+    });
+    
+    /*server.route({
+       method: 'GET',
+        path: './styles/*',   // http://localhost:8000/styles/dsjsdjds/style4.css
+         handler: function (request, reply) {
+           directory: {
+               
+               path: "styles",
+               listing: false
+            }
+        });*/
+
+      server.route({
+        method: 'GET',
+        path: '/2',
+        handler: function (request, reply) {
+//            reply.file('./index.html');
+            reply.file("./index2.html");
+          
+        }
+    });
+     
 });
 
 server.start((err) => {
