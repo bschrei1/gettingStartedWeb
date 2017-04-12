e8daa2e4ed85b93099bc10330d2db7183a0cf26e
wuq282 <ben.schreiber@capitalone.com>
2017-04-12 18:49:26 -0400

    condensed some of the files

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 'use strict';
-
 const Hapi = require('hapi');
 const Path = require('path');
 
@@ -7,121 +6,49 @@ const server = new Hapi.Server({
     connections: {
         routes: {
             files: {
-                relativeTo: Path.join(__dirname,'')
+                relativeTo: Path.join(__dirname, '')
             }
         }
     }
 });
-server.connection({ port: process.env.PORT||8000 });
-
-
+server.connection({ port: process.env.PORT || 8000 });
 
 server.register(require('inert'), (err) => {
-
     if (err) {
         throw err;
     }
-
     server.route({
         method: 'GET',
         path: '/',
-        handler:{
+        handler: {
             file: Path.join(__dirname, 'public/index.html')
         }
     });
-    
     server.route({
         method: 'GET',
         path: '/scripts/{filename}',
-        handler:{
-            directory:
-                {
-                    path: 
-                    Path.join(__dirname, 'scripts')
-                }
-        }
-    });
-    
-    server.route({
-        method:'GET',
-        path:'/styles/{filename}',
-        handler:{directory:{path: Path.join(__dirname, 'styles')}}
-    });
-    
-   /*server.route({
-       method: 'GET',
-        path: '/hello',   // http://localhost:8000/other.html
-         handler: function (request, reply) {
-           directory: {
-               path: ".",
-               listing: false
-           }
-        }
-    });*/
-    
-    server.route({
-        method: 'GET',
-        path: '/styles.css',
         handler: {
-            file: './styles.css'
-        }
-    });
-    server.route({
-        method: 'GET',
-        path: '/index.js',
-        handler: {
-            file: './index.js'
-        }
-    });
-    server.route({
-        method: 'GET',
-        path: '/node_modules/{filename}',
-        handler:{directory:{path: Path.join(__dirname, 'node_modules/')}}
-    });
-    
-    /*server.route({
-       method: 'GET',
-        path: './styles/*',   // http://localhost:8000/styles/dsjsdjds/style4.css
-         handler: function (request, reply) {
-           directory: {
-               
-               path: "styles",
-               listing: false
+            directory:{
+                path:Path.join(__dirname, 'scripts')
             }
-        });*/
-
-      server.route({
-        method: 'GET',
-        path: '/2',
-        handler: function (request, reply) {
-//            reply.file('./index.html');
-            reply.file("./index2.html");
-          
         }
     });
-     
+    server.route({
+        method: 'GET',
+        path: '/styles/{filename}',
+        handler: { 
+            directory: { 
+                path: Path.join(__dirname, 'styles') 
+            } 
+        }
+    });
 });
 
 server.start((err) => {
-
     if (err) {
         throw err;
     }
     console.log(`Server running at: ${server.info.uri}`);
 });
 
-/*'use strict';
-
-const Hapi = require('hapi');
-
-const server = new Hapi.Server();
-server.connection({ port: process.env.PORT ||3000 });
-
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log(`Server running at: ${server.info.uri}`);
-});*/
 
